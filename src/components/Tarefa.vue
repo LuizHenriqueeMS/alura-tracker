5ae4a4e7a6ab1c313801cf272a7bb89e059c466b
Luiz Henrique <luiz.henr0710@gmail.com>
2023-01-06 14:24:52 -0300

    Adicionando base para listas

---
+++ src/components/Tarefa.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="box">
+        <div class="columns">
+            <div class="column is-7">
+                descricao da Tarefa
+            </div>
+            <div class="column">
+                <Cronometro :TempoEmSegundos="15"/>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+
+    import Cronometro from './Cronometro.vue';
+    import { defineComponent } from 'vue';
+
+    export default defineComponent({
+        name:'MinhasTarefas',
+        components:{
+            Cronometro
+        }
+    })
+
+</script>
+
+<style scoped>
+    .box{
+        background : #FAF0CA;
+    }
+</style>
