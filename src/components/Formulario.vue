7bc56bd16f93f169b2b2a910f002e630a34b6e2f
Luiz Henrique <luiz.santos@compsis.com.br>
2023-01-06 09:29:58 -0300

    adicionando componentes

---
+++ src/components/Formulario.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="box">
+        <div class="columns">
+            <div class="columns is-8" role="form" aria-label="Formulário para criação de uma nova tarefa">
+                <input type="text" class="input" placeholder="Qual tarefa deseja iniciar?">
+            </div>
+            <div class="columns">
+                <section>
+                    <strong>
+                        00:00:00
+                    </strong>
+                    <button class="button">
+                        <span class="icon">
+                            <i class="fas fa-play"></i>
+                        </span>
+                        <span>play</span>
+                    </button>
+                    <button class="button">
+                        <span class="icon">
+                            <i class="fas fa-stop"></i>
+                        </span>
+                        <span>stop</span>
+                    </button>
+                </section>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+
+    import { defineComponent } from 'vue';
+
+    export default defineComponent({
+        name: 'MeuFormulario'
+    })
+
+</script>
+
+<style>
+
+</style>
